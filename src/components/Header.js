c948932fa2ba95bc2b76d7521ab529d07b6bb716
portiaportia <portiaplante@gmail.com>
2026-05-21 09:14:51 -0400

    added react skeleton

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <h1>Hi</h1>
+    )
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+const basename = "/803trees";
+
+const App = () => {
+  return (
+      <BrowserRouter basename={basename}>
+        <Routes>
+          <Route path = "/" element = {<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
